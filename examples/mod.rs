44cbddc60060218582c84bad8a6f9aa1a06d38bf
amphetaminefather <158784347+Silene0259@users.noreply.github.com>
2024-11-09 14:47:50 -0800

    Add Errors

--- examples/mod.rs
+++ examples/mod.rs
@@ -1,4 +1,3 @@
 pub mod primitives;
-
 pub mod keys;
 pub mod lattice;
--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,3 +1,41 @@
+use thiserror::Error;
+
+#[derive(Error,Debug)]
 pub enum BorneoErrors {
-    
+    // [0x0000] Encoding Errors
+    #[error("Encoding Error Found In {Context:?} Using The Encoding {Encoding:?} With The Following Message: {Message}")]
+    EncodingError {
+        Encoding: EncodingTypesErrors,
+        Context: ContextTypeErrors,
+        Message: String, 
+    },
+
+    // [0x0001] Serialization Errors
+    #[error("Serialization Error In {Serde:?} For Context {Context:?} With Message: {Message}")]
+    SerializationError {
+        Serde: SerializationTypeErrors,
+        Context: ContextTypeErrors,
+        Message: String,
+    }
+}
+
+#[derive(Debug)]
+pub enum EncodingTypesErrors {
+    HEX,
+    BASE32z,
+    BASE58,
+    BASE64,
+}
+
+#[derive(Debug)]
+pub enum ContextTypeErrors {
+    BlockID,
+    BorneoAccount,
+}
+
+#[derive(Debug)]
+pub enum SerializationTypeErrors {
+    JSON,
+    YAML,
+    BINCODE,
 }
